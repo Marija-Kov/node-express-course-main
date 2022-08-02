b6200bee2450505e2bcb5652ee501263e3e2b394
Marija K <kccmrj@outlook.com>
2022-08-02 06:39:42 +0200

    node practice

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,2 +1,36 @@
-console.log('Task Manager App')
-//mongodb+srv://<username>:<password>@keechcluster.vudhnux.mongodb.net/?retryWrites=true&w=majority
+
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect');
+require('dotenv').config();
+//middleware 
+
+app.use(express.json());
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager')
+})
+
+
+app.use('/api/v1/tasks', tasks)
+
+//get all - get
+//create new - post
+//get single - get  ... :id
+//update - patch/put ... :id
+//delete - delete ... :id
+
+const port = 3004;
+
+const start = async () => {
+    try {
+       await connectDB(process.env.MONGO_URI);  
+       app.listen(port, console.log(`server is listening ${port}`));
+    } catch (error) {
+        console.log(`ERROR! ${error}`)
+    }
+}; 
+
+start()
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,33 @@
+// GET, POST, PUT/PATCH, DELETE controls live here
+const Task = require('../models/Task');
+
+const getAllTasks = (req, res) => {
+    res.send('all tasks')
+};
+
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+};
+
+const getTask = (req, res) => {
+    res.json({id:req.params.id})
+};
+
+const updateTask = (req, res) => {
+    res.send('update')
+};
+
+const deleteTask = (req, res) => {
+    res.send('delete task')
+};
+
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+};
